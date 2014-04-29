f21b1a37d3d10a9d2c04c6c25d70725caed8b382
Otso Rajala <otjura@utu.fi>
2014-04-29 12:38:15 +0300

    sitä varten on tuo NavigatorUI ja sen init mistä kaikki lähtee, joniseni.

---
+++ kori/src/com/kori/frontend/NavigatorUI.java
@@ -0,0 +1,35 @@
+package com.kori.frontend;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.ui.UI;
+
+public class NavigatorUI extends UI {
+	
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = NavigatorUI.class)
+	public static class Servlet extends VaadinServlet {
+		
+	}
+	
+	protected static Navigator navigator;
+	protected static final String LOGIN = "login";
+	protected static final String CONTENT = "content";
+	protected static final String REGISTER = "register";
+	
+	@Override
+	protected void init(VaadinRequest request) {
+		navigator = new Navigator(this,this);
+		navigator.addView(LOGIN, new Login());
+		navigator.addView(CONTENT,new Content());
+		navigator.addView(REGISTER, new Register());
+		
+		//aloitetaan login ruudusta
+		navigator.navigateTo("login");
+		
+	}
+}
